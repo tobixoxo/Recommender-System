90bd73c350a107221981958cbdbaebd8809faea8
tobixoxo <sanjeev.singh.rawat28@gmail>
2022-05-24 14:52:34 +0530

    stored preprocessed df in HDF format for faster execution

--- collaborative_filtering/collaborative_filtering.py
+++ collaborative_filtering/collaborative_filtering.py
@@ -3,6 +3,22 @@ from warnings import simplefilter
 from server.models import Movie, UserRatings
 import pandas as pd
 
+def store_df_as_HDF():
+    print("querying userratings\n")
+    data_list = [make_list(ur) for ur in UserRatings.query.all()]
+
+    rating_dataframe = pd.DataFrame(data_list, columns = ['userId','title','rating'])
+    print("making df of user ratings\n")
+    user_ratings = rating_dataframe.pivot_table(index = ['userId'],columns = ['title'], values= 'rating')
+    #not dropping userrating, resulting into key error
+    user_ratings = user_ratings.fillna(0)
+    item_similarity_df = user_ratings.corr(method = 'pearson')
+    print("item_similarity matrix calculated\n")
+    df_store = pd.HDFStore('processed_data.h5')
+    df_store['preprocessed_df'] = item_similarity_df
+    df_store.close()
+    print("item_similarity matrix stored!\n")
+
 def watch_history_for_user(uid):
         return [(Movie.query.get(watched_movie.movie_id).title ,watched_movie.rating) 
         for watched_movie in 
@@ -22,24 +38,17 @@ def get_similar_movies(movie, user_rating, item_similarity_df):
 
 def make_recommendations(user_id):
     print("reached colb filtering")
-    data_list = [make_list(ur) for ur in UserRatings.query.all()]
+    data_store = pd.HDFStore('processed_data.h5')
+    item_similarity_df =  data_store['preprocessed_df']
 
-    rating_dataframe = pd.DataFrame(data_list, columns = ['userId','title','rating'])
-
-    user_ratings = rating_dataframe.pivot_table(index = ['userId'],columns = ['title'], values= 'rating')
-
-    #   NOTE (DONE) : DO DROP USERS WITH LESS THAN threshold RATINGS
-    user_ratings = user_ratings.dropna(thresh = 10, axis = 1).fillna(0)
-    
-    item_similarity_df = user_ratings.corr(method = 'pearson')
-    # print(item_similarity_df, "\n\n\n")
-
-    print("item_similarity matrix calculated!")
-    real_watcher = watch_history_for_user(user_id)[:5]
+    real_watcher = watch_history_for_user(user_id)
+    print("watcher user history \n")
+    # for i in real_watcher:
+    #     print(i)
 
     similar_movies0 = pd.DataFrame()
     for movie, rating in real_watcher:
-        print(movie," calculation \n")
+        # print(movie," calculation \n")
         similar_movies0 = similar_movies0.append(get_similar_movies(movie, rating, item_similarity_df), ignore_index=True)
 
     similar_movies = similar_movies0
@@ -48,11 +57,11 @@ def make_recommendations(user_id):
 
     similar_movies = similar_movies.sum().sort_values(ascending = False).head(10)
 
-    print(similar_movies,"\n\n\n")
+    # print(similar_movies,"\n\n\n")
 
     similar_movies_dict = similar_movies.to_dict()
     similar_movies_names = similar_movies_dict.keys()
-    print(similar_movies_names,"\n\n\n")
+    # print(similar_movies_names,"\n\n\n")
 
     # NOTE : head of list = 10
     return similar_movies_names
